f905bb066f543fa3ba21bf81c9bc8467e27ecb15
QARosa <rosalijustino@hotmail.com>
2025-05-21 19:50:55 -0300

    Testes

--- ServerRest/test/performance/utils.js
+++ ServerRest/test/performance/utils.js
@@ -1,1 +1,3 @@
-export const BASEURL = 'http://localhost:3000'
+// export const BASEURL = 'http://localhost:3000' testes local
+//testes na aws
+export const BASEURL = 'http://ec2-3-149-8-46.us-east-2.compute.amazonaws.com:3000'
